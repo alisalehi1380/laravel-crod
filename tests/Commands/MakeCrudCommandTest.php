5f7ad2e114d2c54e849da1b051895e4d4c11fae8
Milwad <milwad.dev@gmail.com>
2024-04-15 17:54:59 +0330

    wip

--- tests/Commands/MakeCrudCommandTest.php
+++ tests/Commands/MakeCrudCommandTest.php
@@ -3,8 +3,20 @@
 namespace Milwad\LaravelCrod\Tests\Commands;
 
 use Milwad\LaravelCrod\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class MakeCrudCommandTest extends TestCase
 {
+    /**
+     * Test crud files created successfully.
+     */
+    #[Test]
+    public function crud_files_created_successfully(): void
+    {
+        $this->artisan('crud:make', ['name' => 'Product'])
+            ->expectsQuestion('Do you want something extra?', 5)
+            ->assertSuccessful();
 
+
+    }
 }
