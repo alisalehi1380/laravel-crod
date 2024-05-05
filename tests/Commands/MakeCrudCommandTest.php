69690706fb94ab831597640f9dc6319bd8d65146
Milwad <milwad.dev@gmail.com>
2024-05-05 15:57:33 +0330

    add `crud_files_created_successfully_with_repository` test

--- tests/Commands/MakeCrudCommandTest.php
+++ tests/Commands/MakeCrudCommandTest.php
@@ -56,6 +56,23 @@ class MakeCrudCommandTest extends TestCase
     }
 
     /**
+     * Test crud files created successfully with repository.
+     */
+    #[Test]
+    public function crud_files_created_successfully_with_repository(): void
+    {
+        $this->artisan('crud:make', ['name' => 'Product'])
+            ->expectsQuestion('Do you want something extra?', 3)
+            ->expectsQuestion('Do you want something extra?', 0)
+            ->assertSuccessful()
+            ->expectsOutput('Crud files successfully generated...');
+
+        $this->ensureCrudFileCreated();
+
+        $this->assertFileExists(app_path('Repositories/ProductRepo.php'));
+    }
+
+    /**
      * Ensure the crud files successfully created.
      */
     protected function ensureCrudFileCreated(): void
