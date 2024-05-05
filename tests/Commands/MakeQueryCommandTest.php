d35a4ffde5a7a1c4e3901d2eb71e3cd85ac7744d
Milwad <milwad.dev@gmail.com>
2024-05-05 16:05:38 +0330

    Update MakeQueryCommandTest.php

--- tests/Commands/MakeQueryCommandTest.php
+++ tests/Commands/MakeQueryCommandTest.php
@@ -2,9 +2,12 @@
 
 namespace Milwad\LaravelCrod\Tests\Commands;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Milwad\LaravelCrod\Tests\TestCase;
 
 class MakeQueryCommandTest extends TestCase
 {
+    use RefreshDatabase;
 
+    
 }
