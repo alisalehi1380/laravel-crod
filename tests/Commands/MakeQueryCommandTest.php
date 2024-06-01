1d3b2ddc1cbfcb58708504b9a93caa4db73d790b
Milwad Khosravi <milwad.dev@gmail.com>
2024-06-01 16:52:28 +0330

    add `all_files_are_exists_with_correct_data` test

---
+++ tests/Commands/MakeQueryCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Milwad\LaravelCrod\Tests\Commands;
+
+use Milwad\LaravelCrod\Tests\TestCase;
+use Orchestra\Testbench\Concerns\InteractsWithPublishedFiles;
+use PHPUnit\Framework\Attributes\Test;
+
+class MakeQueryCommandTest extends TestCase
+{
+    use InteractsWithPublishedFiles;
+
+    /**
+     * Test all files are exists with correct data.
+     */
+    #[Test]
+    public function all_files_are_exists_with_correct_data(): void
+    {
+        $this->artisan('crud:make', ['name' => 'Product'])
+            ->expectsQuestion('Do you want something extra?', 0)
+            ->assertSuccessful()
+            ->expectsOutput('Crud files successfully generated...');
+
+        // Model
+        $this->assertFileContains([
+            '<?php',
+            'namespace App\Models;',
+            'class Product extends Model',
+        ], 'app/Models/Product.php');
+
+        // Controller
+        $this->assertFileContains([
+            '<?php',
+            'namespace App\Http\Controllers;',
+            'class ProductController extends Controller',
+        ], 'app/Http/Controllers/ProductController.php');
+
+        // Requests
+        $this->assertFileContains([
+            '<?php',
+            'namespace App\Http\Requests;',
+            'class ProductStoreRequest extends FormRequest',
+            'public function authorize(): bool',
+            'public function rules(): array',
+        ], 'app/Http/Requests/ProductStoreRequest.php');
+        $this->assertFileContains([
+            '<?php',
+            'namespace App\Http\Requests;',
+            'class ProductUpdateRequest extends FormRequest',
+            'public function authorize(): bool',
+            'public function rules(): array',
+        ], 'app/Http/Requests/ProductUpdateRequest.php');
+
+        // View
+        $this->assertFileContains(['{{-- Start to write code - milwad-dev --}}'], 'resources/views/products/index.blade.php');
+        $this->assertFileContains(['{{-- Start to write code - milwad-dev --}}'], 'resources/views/products/create.blade.php');
+        $this->assertFileContains(['{{-- Start to write code - milwad-dev --}}'], 'resources/views/products/edit.blade.php');
+    }
+}
